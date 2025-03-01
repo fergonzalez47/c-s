cd74a7fccd4e2e34856a3f9871d27dc4dff37198
Fernando Gonzalez <fernandogonzalezvaldz@gmail.com>
2025-03-01 18:05:39 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,54 @@
+
+import { apiFetch, displayServices, inputPhoneNumber, displayIconsServices, addRequestBtnService } from "./utils.js";
+import { displayInf } from "./thanks.js";
+import { renderServices } from "./services.js";
+import { initIntersectionObserver } from "./ioo.js";
+
+const servicesPath = "scripts/features.json";
+
+
+//check if the input number exists
+window.addEventListener("DOMContentLoaded", async () => {
+
+    if (document.getElementById("key-features")) {
+        const services = await apiFetch(servicesPath);
+        const keyFeatures = document.getElementById("key-features");
+        displayServices(services.services, keyFeatures);
+    }
+
+    if (document.getElementById("service-icons")) {
+        const servicesIcons = await apiFetch(servicesPath);
+        const iconsContainer = document.getElementById("service-icons");
+        displayIconsServices(servicesIcons.iconServices, iconsContainer);
+    }
+
+
+
+    
+    if (document.getElementById("phone")) {
+        inputPhoneNumber();
+    }
+
+    if (document.getElementById("received-inf")) {
+        displayInf();
+    }
+
+    const timestampInput = document.getElementById("timestamp");
+    // Asigna el valor actual de tiempo (timestamp en milisegundos) al campo oculto del formulario mio
+    if (timestampInput) {
+        timestampInput.value = new Date().getTime();
+    }
+
+    const servicesContainer = document.getElementById('services');
+    if (servicesContainer) {
+        renderServices();
+    }
+
+
+    const ServiceContainerInfoList = document.querySelectorAll(".services-container-inf");
+    if (ServiceContainerInfoList) {
+        addRequestBtnService(ServiceContainerInfoList);
+    }
+
+    initIntersectionObserver();
+});
---
+++ scripts/utils.js
@@ -0,0 +1,158 @@
+export async function apiFetch(url) {
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            const errorMessage = await response.text();
+            throw new Error(`HTTP Error: ${response.status} - ${errorMessage}`);
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error("Fetch error:", error);
+    }
+}
+
+
+
+export function createServiceElement(service) {
+    const div = document.createElement("div");
+    div.classList.add("key-feature-item");
+
+    const title = document.createElement("h3");
+    title.textContent = service.title;
+
+    const iconContainer = document.createElement("div");
+    iconContainer.classList.add("icon-container");
+
+    const icon = document.createElement("img");
+    icon.setAttribute("src", service.icon);
+    icon.setAttribute("alt", `image of ${service.name}`);
+    icon.setAttribute("loading", "lazy");
+    icon.classList.add("main-icon");
+
+    const description = document.createElement("p");
+    description.textContent = service.description;
+
+    iconContainer.appendChild(icon);
+    div.appendChild(iconContainer);
+    div.appendChild(title);
+    div.appendChild(description);
+
+    return div;
+}
+
+export function displayServices(services, container) {
+    services.forEach(function (service) {
+        const serviceElement = createServiceElement(service);
+        container.appendChild(serviceElement);
+    });
+}
+
+
+
+export function inputPhoneNumber() {
+
+    document.getElementById('phone').addEventListener('input', function (e) {
+        let value = e.target.value.replace(/\D/g, ''); // delete excess of characters
+        if (value.length > 10) value = value.slice(0, 10); // Máx 10 dígits
+
+        let formattedValue = '';
+        if (value.length > 6) {
+            formattedValue = `(${value.slice(0, 3)}) ${value.slice(3, 6)}-${value.slice(6)}`;
+        } else if (value.length > 3) {
+            formattedValue = `(${value.slice(0, 3)}) ${value.slice(3)}`;
+        } else if (value.length > 0) {
+            formattedValue = `(${value}`;
+        }
+        e.target.value = formattedValue;
+    });
+
+}
+
+
+
+export function displayModal(card, htmlElement) {
+    htmlElement.innerHTML = "";
+    htmlElement.innerHTML = `
+    <button id="closeModal">❌</button>
+    <h2>${card.title}</h2>
+    <p>${card.description}</p>
+    `;
+
+    htmlElement.showModal();
+
+    document.getElementById("closeModal").addEventListener("click", () => {
+        htmlElement.classList.add("closing");
+        setTimeout(() => {
+            htmlElement.close();
+            htmlElement.classList.remove("closing");
+        }, 350);
+    });
+
+
+}
+
+
+
+
+// display icons in home
+
+
+export function createIconElement(service) {
+    const div = document.createElement("div");
+    div.classList.add("icon-service-card");
+
+    const title = document.createElement("h5");
+    title.textContent = service.title;
+
+    const iconContainer = document.createElement("div");
+    iconContainer.classList.add("icon-services-container");
+
+    const icon = document.createElement("img");
+    icon.setAttribute("src", service.icon);
+    icon.setAttribute("alt", `image of ${service.title}`);
+    icon.setAttribute("loading", "lazy");
+    icon.classList.add("home-services-icon");
+
+    div.addEventListener("click", () => {
+        window.location.href = "./services/";
+    })
+
+    iconContainer.appendChild(icon);
+    div.appendChild(iconContainer);
+    div.appendChild(title);
+
+    return div;
+}
+
+export function displayIconsServices(services, container) {
+    services.forEach(function (service) {
+        const serviceElement = createIconElement(service);
+        container.appendChild(serviceElement);
+    });
+}
+
+
+
+
+export function addRequestBtnService(containerList) {
+
+
+    containerList.forEach(container => {
+        const requestBtn = document.createElement("a");
+        requestBtn.textContent = "Request a Free Estimate";
+        requestBtn.classList.add("cta-3");
+        requestBtn.href = "https://wa.me/13854360911?text=" +
+            encodeURIComponent("Hi, I am interested in requesting an estimate for cleaning services. Could you please provide more details about... ?");
+        
+        requestBtn.target = "_blank";
+        requestBtn.rel = "noopener noreferrer";
+
+
+        container.appendChild(requestBtn);
+    });
+}
+
